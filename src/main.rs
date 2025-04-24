aae798e84e99c2f1f6ae3bb13e78fde92df2a569
archlaptop <iska.virtanen@student.oulu.fi>
2025-04-24 10:36:22 +0300

    the shell handles args now

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,14 @@ fn main() {
     loop{
         print!("> ");
         let _ = std::io::stdout().flush();
+
         let mut input = String::new();
         std::io::stdin().read_line(&mut input).unwrap();
-
+        // everything after the first whitespace char
+        // is interpreted as args to the command
+        let parts = input.trim().split_whitespace();
         let command = input.trim();
-
+        let _args = parts;
         let mut child = std::process::Command::new(command).spawn().unwrap();
 
         let _ = child.wait();
