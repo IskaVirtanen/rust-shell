6dbe5c44d5012d7dab9cfd44610198159b945dd0
archlaptop <iska.virtanen@student.oulu.fi>
2025-04-24 16:16:59 +0300

    Last functionality added

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,68 @@
-use std::io::Write;
-use std::process::Command;
-use std::env;
+use std::io::{stdin, stdout, Write};
 use std::path::Path;
+use std::process::{Child,Command,Stdio};
+use std::env;
 fn main() {
     loop{
         print!("> ");
-        let _ = std::io::stdout().flush();
+        stdout().flush().unwrap();
 
         let mut input = String::new();
-        std::io::stdin().read_line(&mut input).unwrap();
-        // everything after the first whitespace char
-        // is interpreted as args to the command
-        let mut parts = input.trim().split_whitespace();
-        let command = parts.next().unwrap();
-        let _args = parts;
+        stdin().read_line(&mut input).unwrap();
 
-        match command {
-            "cd" => {
-                // default to '/' as new directory if one was not provided
-                let new_dir = _args.peekable().peek().map_or("/",|x| *x);
-                let root = Path::new(new_dir);
-                if let Err(e) = env::set_current_dir(&root){
-                    eprintln!("{}",e);
+        let mut commands = input.trim().split(" | ").peekable();
+        let mut previous_command = None;
+
+        while let Some(command) = commands.next(){
+
+            // everything after the first whitespace char
+            // is interpreted as args to the command
+            let mut parts = command.trim().split_whitespace();
+            let command = parts.next().unwrap_or("");
+            let _args = parts;
+
+		        match command {
+                "cd" => {
+                    // default to '/' as new directory if one was not provided
+                    let new_dir = _args.peekable().peek().map_or("/",|x| *x);
+                    let root = Path::new(new_dir);
+                    if let Err(e) = env::set_current_dir(&root){
+                        eprintln!("{}",e);
+                    }
+                    previous_command = None;
+                },
+                "exit" => return,
+                command => {
+                   let stdin = previous_command.map_or(
+                            std::process::Stdio::inherit(),
+                                |output: Child|{ Stdio::from(output.stdout.unwrap())
+
+                                });
+                   let _stdout = if commands.peek().is_some(){
+                       Stdio::piped()
+                   }else{
+                        Stdio::inherit()
+                   };
+
+                   let _output = Command::new(command)
+                       .args(_args)
+                       .stdin(stdin)
+                       .stdout(_stdout)
+                       .spawn();
+                    match _output{
+                        Ok(output) =>{
+                            previous_command = Some(output);
+                        }
+                        Err(e) => {
+                            previous_command = None;
+                            eprintln!("{}",e);
+                        }
+                   }; 
                 }
-            },
-            "exit" => return,
-            command => {
-                let child = Command::new(command).args(_args).spawn(); 
-
-                match child{
-                    Ok(mut child) => {let _ = child.wait();},
-                    Err(e) => eprintln!("{}",e),
-                };
             }
+        }
+        if let Some(mut final_command) = previous_command{
+            final_command.wait().unwrap();
         }
     }
 }
