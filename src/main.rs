9c822970d215283ae3db78cdc2d4382b19f63a2c
archlaptop <iska.virtanen@student.oulu.fi>
2025-04-24 11:23:43 +0300

    Fixed annoying bug

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::io::Write;
-
-
+use std::process::Command;
+use std::env;
+use std::path::Path;
 fn main() {
     loop{
         print!("> ");
@@ -10,12 +11,29 @@ fn main() {
         std::io::stdin().read_line(&mut input).unwrap();
         // everything after the first whitespace char
         // is interpreted as args to the command
-        let parts = input.trim().split_whitespace();
-        let command = input.trim();
+        let mut parts = input.trim().split_whitespace();
+        let command = parts.next().unwrap();
         let _args = parts;
-        let mut child = std::process::Command::new(command).spawn().unwrap();
 
-        let _ = child.wait();
-        
+        match command {
+            "cd" => {
+                // default to '/' as new directory if one was not provided
+                let new_dir = _args.peekable().peek().map_or("/",|x| *x);
+                let root = Path::new(new_dir);
+                if let Err(e) = env::set_current_dir(&root){
+                    eprintln!("{}",e);
+                }
+            },
+            "exit" => return,
+            command => {
+                let child = Command::new(command).args(_args).spawn(); 
+
+                match child{
+                    Ok(mut child) => {let _ = child.wait();},
+                    Err(e) => eprintln!("{}",e),
+                };
+            }
+        }
     }
 }
+
